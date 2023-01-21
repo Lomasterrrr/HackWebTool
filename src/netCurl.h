d88316d674661bc17d66c08b421400ee7c869cdc
lomaster <119946584+Lomasterrrr@users.noreply.github.com>
2023-01-21 22:24:56 +0300

    Delete src directory

--- src/netCurl.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-size_t ClearWrite(void *buffer, size_t size, size_t nmemb, void *userp);
-long ResponseCode(std::string link);
-std::string GetIP(std::string link);
-
